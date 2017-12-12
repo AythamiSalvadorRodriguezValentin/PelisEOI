b3d06ffe511b811aed3afcce17aa36719f6d9a7e
Aythami Rodriguez Valentin <aythami94srv@outlook.com>
2017-12-12 10:16:44 +0000

    se han añadido las clases al films container y se ha añadido un nuevo componente para hacer el slider

---
+++ components/ngSliderOwn/ngSliderOwn.component.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+    ////////////////////////////////////////////////////////////
+    // Usage:
+    // 
+    // Creates:
+    // 
+    ////////////////////////////////////////////////////////////
+    angular
+        .module('PelisEOI')
+        .component('ngSliderOwn', {
+            templateUrl: '/components/ngSliderOwn/ngSliderOwn.html',
+            controller: NgSliderOwnController,
+            controllerAs: '$ctrl',
+            bindings: {
+                slider: '=',
+            },
+        });
+    ////////////////////////////////////////////////////////////
+    NgSliderOwnController.$inject = [];
+    function NgSliderOwnController() {
+        var $ctrl = this;
+        ////////////////////////////////////////////////////////////
+        $ctrl.$onInit = function() { };
+        $ctrl.$onChanges = function(changesObj) { };
+        $ctrl.$onDestroy = function() { };
+    }
+})();
