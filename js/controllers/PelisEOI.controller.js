349360be4306cb5d70eac09d52e599d6fd9a7004
Aythami Rodriguez Valentin <aythami94srv@outlook.com>
2017-12-14 20:01:56 +0000

    se ha refactorizado el controller y corregido algunos errores

--- js/controllers/PelisEOI.controller.js
+++ js/controllers/PelisEOI.controller.js
@@ -5,17 +5,20 @@
         .module('PelisEOI')
         .controller('PelisEOIController', PelisEOIController);
 
-    PelisEOIController.$inject = ['$location','OmdbIDServerProvider','TheMovieDBServerProvider'];
-    function PelisEOIController($location,OIDSP,TMDBSP) {
+    PelisEOIController.$inject = ['$location','InterfazServerFactory'];
+    function PelisEOIController($location,InterSF) {
         let vm = this;
         ///////////////////////// VAR VM //////////////////////////
         vm.films = {};
         vm.film = {};
         vm.search = {};
         vm.genreList = [];
-        vm.load = false;
         vm.navList = [];
         vm.view = '';
+        vm.orderBy = [];
+        vm.slider = {};
+        vm.timeout = {};
+        vm.load = false;
         vm.viewFilm = false;
         ///////////////////////////////////////////////////////////
         /////////////////////// FUCTION VM ////////////////////////
@@ -23,33 +26,35 @@
         vm.getMovies = getMovies;
         vm.changeView = changeView;
         vm.selectGenre = selectGenre;
+        vm.resetFilter = resetFilter;
+        vm.changeFilter = changeFilter;
         vm.checkGenreButton = checkGenreButton;
         /////////////////////////// INIT //////////////////////////////
         activate();
         /////////////////////// FUCTION $INIT /////////////////////////
         function activate() {
-            changeView('Descubrir');
-            vm.genreList = OIDSP.getGenres();
-            vm.search = {title:'',genre:'',year:'',type:'movie',language:'es-ES',page:1};
+            vm.orderBy = InterSF.getOrderDataBy();
             vm.navList = ['Descubrir','Próximamente','Mis favoritas','Para más tarde','Vistas'];
+            vm.search = {title:'',genre:'',year:'',type:'',language:'',sort_by:'',page:1};
+            vm.view = vm.navList[0];
+            resetFilter();
+            InterSF
+                .getMoviesData(vm.search,'genres')
+                .then(loaded => vm.genreList = loaded)
+                .catch(e => {console.error(e)});
         };
         /////////////////////// FUCTION $VIEW /////////////////////////
         function changeView(nav){
             vm.view = nav;
-            if (nav == 'Descubrir') {
-                TMDBSP
-                    .getMoviesDBPopular(vm.search)
-                    .then(loaded => {
-                        vm.films = loaded;
-                        vm.load = false;
-                    }).catch(e => console.error(e));
-            } else if(nav == 'Próximamente'){
+            if (nav == vm.navList[0]) {
+                resetFilter();
+            } else if(nav == vm.navList[1]){
                 vm.films = {};
-            } else if(nav == 'Mis favoritas'){
+            } else if(nav == vm.navList[2]){
                 
-            } else if(nav == 'Para más tarde'){
+            } else if(nav == vm.navList[3]){
                 
-            } else if(nav == 'Vistas'){
+            } else if(nav == vm.navList[4]){
                 
             }
         }
@@ -57,67 +62,83 @@
         function getMovies(more){
             if (vm.search.title.length >= 2) {
                 vm.load = true;
-                vm.search.type = 'movie';
-                vm.search.language = 'es-ES';
                 if(more === 'Y'){
                     vm.search.page++;
-                    TMDBSP
-                        .getMoviesDBSearch(vm.search)
-                        .then(loaded => {
-                            vm.films = loaded;
-                            vm.load = false;
-                        }).catch(e => console.error(e));
                 } else {
                     vm.search.page = 1;
                     vm.films = [];
-                    TMDBSP
-                        .getMoviesDBSearch(vm.search)
-                        .then(loaded => {
-                            vm.films = loaded;
-                            vm.load = false;
-                        }).catch(e => console.error(e));
-                }    
+                }
+                InterSF
+                    .getMoviesData(vm.search,'search')
+                    .then(loaded => {
+                        vm.films = loaded;
+                        vm.load = false;
+                    }).catch(e => console.error(e));
             }
         };
-        /////////////////////// FUCTION GENRE  ////////////////////////
+        /////////////////////// FUCTION FILTER ////////////////////////
+        function changeFilter(){
+            clearTimeout(vm.timeout.filter);
+            vm.timeout.filter = setTimeout(getMovieDiscover,300);
+        }
+        function resetFilter(){
+            /* Genres */
+            vm.search.genre = '';
+            /* Search */
+            vm.search.language = 'es-ES';
+            vm.search.sort_by = vm.orderBy[1];
+            /* Slider */
+            vm.slider.minYear = 1950;
+            vm.slider.minYearValue = 2000;
+            vm.slider.maxYear = 2050;
+            vm.slider.maxYearValue = 2000;
+            /* Popular */
+            InterSF
+                .getMoviesData(vm.search,'popular')
+                .then(loaded => {
+                    vm.films = loaded;
+                    vm.load = false;
+                }).catch(e => console.error(e));
+        }
+        /////////////////////// FUCTION DISCOVER ////////////////////////
+        function getMovieDiscover(){
+            vm.search.sort_by = vm.search.sort_by;
+            vm.search.release_date_gte = vm.slider.minYearValue + '-01-01';
+            vm.search.release_date_lte = vm.slider.maxYearValue + '-12-31';
+            InterSF
+                .getMoviesData(vm.search,'discover')
+                .then(loaded => {
+                    vm.films = loaded;
+                    vm.load = false;
+                }).catch(e => console.error(e));
+        }
+        /////////////////////// FUCTION GENRE /////////////////////////
         function selectGenre(genre){
             if (vm.search.genre.indexOf(genre) == -1) {
-                if(vm.search.genre.length > 1) vm.search.genre += ', ';
+                if(vm.search.genre.length > 1) vm.search.genre += '%2C';
                 vm.search.genre += genre;
             } else {
-                let arrayGenre = vm.search.genre.split(', ');
+                let arrayGenre = vm.search.genre.split('%2C');
                 for(let i = 0; i < arrayGenre.length; i++){
                     if(arrayGenre[i] == genre) arrayGenre.splice(i,1);
                 }
-                vm.search.genre = arrayGenre.join(', ');
+                vm.search.genre = arrayGenre.join('%2C');
             }
+            changeFilter();
         };
         function checkGenreButton(genre){
             return (vm.search.genre.indexOf(genre) != -1) ? true : false;
         };
-        ///////////////////// FUCTION SHOW VIEW  //////////////////////
+        ///////////////////// FUCTION SHOW VIEW ///////////////////////
         function showFilm(film){
-            film.language = 'es-ES';
+            film.language = vm.search.language;
+            film.externalID = 'imdb_id';
             film.page = 1;
-            TMDBSP
-                .getMovieDBID(film)
+            InterSF
+                .getMovieDataFull(film)
                 .then(loaded => {
                     vm.film = loaded;
-                }).catch(e => console.error(e));
-            TMDBSP
-                .getMoviesDBSimilar(film)
-                .then(loaded => {
-                    vm.film.similar = loaded;
-                }).catch(e => console.error(e));
-            TMDBSP
-                .getMoviesDBVideo(film)
-                .then(loaded => {
-                    vm.film.video = loaded;
-                }).catch(e => console.error(e));
-            OIDSP
-                .getMovie(film)
-                .then(loaded => {
-                    vm.film.ombdID = loaded;
+                    console.log(vm.film);
                     vm.viewFilm = true;
                 }).catch(e => console.error(e));
         };
