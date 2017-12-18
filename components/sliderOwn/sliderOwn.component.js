a0a36cc1c3f9638d57eee260450d0d3dbd8e1647
Aythami Rodriguez Valentin <aythami94srv@outlook.com>
2017-12-18 13:10:58 +0000

    se esta configurando el slider

--- components/sliderOwn/sliderOwn.component.js
+++ components/sliderOwn/sliderOwn.component.js
@@ -23,13 +23,42 @@
     NgSliderOwnController.$inject = [];
     function NgSliderOwnController() {
         let $ctrl = this;
+        $ctrl.init = true;
+        $ctrl.minDefault = 0;
+        $ctrl.maxDefault = 100;
+        $ctrl.minValueDefault = 25;
+        $ctrl.maxValueDefault = 75;
         ////////////////////////////////////////////////////////////
-        $ctrl.$onInit = function () { initSlider(); };
-        $ctrl.$onChanges = function (changesObj) { };
-        $ctrl.$onDestroy = function () { stopSlider(); };
+        $ctrl.$onInit = function () {
+            configSlider();
+            initSlider();
+        };
+        $ctrl.$onChanges = function (changesObj) {
+            console.log(changesObj);
+        };
+        $ctrl.$onDestroy = function () {
+            stopSlider();
+        };
         ////////////////////////////////////////////////////////////
+        function configSlider() {
+            if ($ctrl.init) {
+                $ctrl.init = false;
+                let containerSizeX = $('.component-slider-general').outerWidth();
+                let mouseXMin = $ctrl.minValueDefault * containerSizeX / 100;
+                let mouseXMax = $ctrl.maxValueDefault * containerSizeX / 100;
+                $('#component-slider-value-min').text(Math.round($ctrl.minValueDefault));
+                $('#component-slider-value-max').text(Math.round($ctrl.maxValueDefault));
+                $('.component-slider-container > .component-slider-progressMin').css('width', (mouseXMin - 7) + 'px');
+                $('.component-slider-container > .component-slider-indicator-min').css('left', (mouseXMin - 7) + 'px');
+                $('.component-slider-container > .component-slider-progressMedium').css('width', (mouseXMax - 7) + 'px');
+                $('.component-slider-container > .component-slider-indicator-max').css('left', (mouseXMax - 7) + 'px');
+                $('.component-slider-indicator-min').css('color', 'white');
+                $('.component-slider-indicator-max').css('color', 'white');
+            }
+        }
         function initSlider() {
             $('.component-slider-general').on('mousedown', function (e) {
+                configSlider();
                 /* Container */
                 let containerSizeX = $(e.currentTarget).outerWidth();
                 let containerXY = $(e.currentTarget).offset();
@@ -76,11 +105,11 @@
             /* Mouse sobre el slider */
             let mouseX = e.pageX - containerXY.left;
             /* value min */
-            let value = mouseX * 100 / containerSizeX;
+            $ctrl.minValue = mouseX * 100 / containerSizeX;
             if (mouseX >= 0 && mouseX <= containerSizeX && posMaxX < 18) {
                 $('.component-slider-container > .component-slider-progressMin').css('width', (mouseX - 7) + 'px');
                 $('.component-slider-container > .component-slider-indicator-min').css('left', (mouseX - 7) + 'px');
-                $('#component-slider-value-min').text(Math.round(value));
+                $('#component-slider-value-min').text(Math.round($ctrl.minValue));
                 $('.component-slider-indicator-min').css('z-index', 6);
                 $('.component-slider-indicator-max').css('z-index', 5);
             }
@@ -101,16 +130,16 @@
             /* Mouse sobre el slider */
             let mouseX = e.pageX - containerXY.left;
             /* value max */
-            let value = mouseX * 100 / containerSizeX;
+            $ctrl.maxValue = mouseX * 100 / containerSizeX;
             if (mouseX >= 0 && mouseX <= containerSizeX && posMinX >= 18) {
                 $('.component-slider-container > .component-slider-progressMedium').css('width', (mouseX - 7) + 'px');
                 $('.component-slider-container > .component-slider-indicator-max').css('left', (mouseX - 7) + 'px');
-                $('#component-slider-value-max').text(Math.round(value));
+                $('#component-slider-value-max').text(Math.round($ctrl.maxValue));
                 $('.component-slider-indicator-min').css('z-index', 5);
                 $('.component-slider-indicator-max').css('z-index', 6);
             }
         };
-        function stopSlider(){
+        function stopSlider() {
             $('.component-slider-container').off('mousedown');
             $('.component-slider-container').off('mousemove');
         };
