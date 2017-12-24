e6cb697e94ed5c1eb11fd2a4429553244868281a
Aythami Rodriguez Valentin <aythami94srv@outlook.com>
2017-12-24 20:21:41 +0000

    correct bugs in register form

--- components/personalForm/personalForm.component.js
+++ components/personalForm/personalForm.component.js
@@ -39,13 +39,28 @@
         };
         //////////////////////// FUCTION CREATE /////////////////////////
         function signCreateUser() {
+            if ($ctrl.user.form.$invalid) {
+                $ctrl.message({ e: 'Ups, los argumentos no son válidos', type: 'error' });
+                $ctrl.push = true;
+                return;
+            }
+            $ctrl.load = true;
             InterSF
                 .firebaseSign($ctrl.user.create, 'create')
                 .then(loaded => {
                     createUser();
                     signUpdateUser();
                 }).catch(e => {
-                    $ctrl.message({ e: $ctrl.mssg, type: 'error' });
+                    $ctrl.load = false;
+                    if (e.message.indexOf('A network error') != -1)
+                        $ctrl.message({ e: 'Ups, no hay conexión a internet', type: 'error' });
+                    else if (e.message.indexOf("Cannot read property 'email' of undefined") != -1)
+                        $ctrl.message({ e: 'Introduce el correo y la contraseña', type: 'error' });
+                    else if (e.message.indexOf("createUserWithEmailAndPassword failed: First argument " + '"email"' + " must be a valid string.") != -1)
+                        $ctrl.message({ e: 'Ups, el correo no es válido', type: 'error' });
+                    else if (e.message.indexOf("createUserWithEmailAndPassword failed: Second argument " + '"password"' + " must be a valid string.") != -1)
+                        $ctrl.message({ e: 'Ups, la contraseña no es válida', type: 'error' });
+                    else $ctrl.message({ e: e.message, type: 'error' });
                     $scope.$apply($ctrl.push = true);
                 });
         };
@@ -57,6 +72,7 @@
                     $ctrl.user.create = {};
                     $ctrl.user.login = {};
                     $ctrl.user.auth = true;
+                    $ctrl.load = false;
                     $scope.$apply($ctrl.user.register = false);
                 }).catch(e => $ctrl.message({ e: $ctrl.mssg, type: 'error' }));
         };
@@ -76,7 +92,7 @@
         function teclado(bool) {
             if (bool) {
                 $('html').on('keydown', (e) => {
-                    /* if (e.keyCode === 13) createUser(); */
+                    if (e.keyCode === 13) $scope.$apply(signCreateUser);
                     if (e.keyCode === 27) $scope.$apply(closeWindows);
                 });
             }
