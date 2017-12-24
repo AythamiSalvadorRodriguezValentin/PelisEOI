35b7e000b2dd9c1a952e925ae732ac6e031517cb
Aythami Rodriguez Valentin <aythami94srv@outlook.com>
2017-12-24 21:19:20 +0000

    correct src in app.config for gh-pages

--- js/app.config.js
+++ js/app.config.js
@@ -4,12 +4,12 @@ function config($routeProvider) {
     $routeProvider
         .when('/', {
             controller: 'PelisEOIController',
-            templateUrl: '../views/PelisEOI.html',
+            templateUrl: '../../views/PelisEOI.html',
             controllerAs: '$films'
         })
         .when('/user', {
             controller: 'PerfilUserController',
-            templateUrl: '../views/PerfilUser.html',
+            templateUrl: '../../views/PerfilUser.html',
             controllerAs: '$user'
         })
         .otherwise({ redirectTo: '/' });
